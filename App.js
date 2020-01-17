abc545f5cc44c6139d8fec54a41b708088d8ed2b
Alexander <alp@jensoft.net>
2020-01-17 10:25:32 +0200

    fixing geolocation

--- App.js
+++ App.js
@@ -32,7 +32,25 @@ async function getPermissions () {
 }
   
  const findCoordinates = () => {
-	const location=	Geolocation.getCurrentPosition();
+   var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  console.log('Your current position is:');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+	const location=	Geolocation.getCurrentPosition(success, error, options);
 console.log(location);
 	};
 
---
+++ components/Permissions.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import {request, PERMISSIONS} from 'react-native-permissions';
+
+
+const Permissions: () => React$Node = () => {
+
+
+    async function getPermissions () {
+   const granted= await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+         if (granted === "granted") {
+      console.log('You can use the location '+granted);
+      findCoordinates();
+      } else {
+      console.log('location permission denied '+granted);
+     }
+    }
+}
+
+export default Permissions;
